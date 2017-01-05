18fbd1c9f920a4a1e57203a6cf6448792c5b15e2
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-05 14:53:02 +0100

    asd

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -32,10 +32,10 @@ public class Player {
         ArrayList<Integer> communityCards = getRank(gameStateObject.getAsJsonArray("community_cards"));
 
         // Put your card observing logic here ------------------------------------
-
-        if (twoHighCards(myCards, stack)) bet = raise(myBet, buyIn, minimumRaise, 200);
         if (oneHighCards(myCards, stack)) bet = raise(myBet, buyIn, minimumRaise, 200);
-        if (highPairs(myCards)) bet = raise(myBet, buyIn, minimumRaise, 200);
+        else if (twoHighCards(myCards, stack)) bet = raise(myBet, buyIn, minimumRaise, 200);
+        else if (highPairs(myCards)) bet = raise(myBet, buyIn, minimumRaise, 200);
+        else if (havePairOnTable(myCards, communityCards)) bet = raise(myBet, buyIn, minimumRaise, 100);
 
         //------------------------------------------------------------------------
 
