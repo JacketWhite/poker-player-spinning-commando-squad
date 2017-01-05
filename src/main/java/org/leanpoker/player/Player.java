8c97215335f84354fb378031d8eac4214448e389
annakertesz <kerteszannanak@gmail.com>
2017-01-05 11:24:58 +0100

    fsd

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -28,7 +28,7 @@ public class Player {
         int buyIn = gameStateObject.get("current_buy_in").getAsInt();
         int myBet = ourPlayer.get("bet").getAsInt();
 
-        return buyIn-myBet;
+        return 100;
     }
 
     public static void showdown(JsonElement game) {
