8d9c1ae0beb8f71b9533eb30d28d10a772b55232
annakertesz <kerteszannanak@gmail.com>
2017-01-05 11:09:12 +0100

    get card values

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,16 +2,23 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement gameState) {
-
+        ArrayList<String> letters = new ArrayList<>(Arrays.asList("J", "D", "K", "A"));
+        List<Integer> cardValues = new ArrayList<Integer>();
         for(JsonElement player: gameState.getAsJsonObject().getAsJsonArray("players")){
             if (player.getAsJsonObject().get("id") == gameState.getAsJsonObject().get("in_action")){
                 for (JsonElement card : player.getAsJsonObject().getAsJsonArray("hole_cards")){
-                    card.getAsJsonObject().get("rank");
+                    String value = String.valueOf(card.getAsJsonObject().get("rank"));
+                    if (letters.contains(value)) cardValues.add(11+letters.indexOf(value));
+                    else cardValues.add(Integer.valueOf(value));
                 }
             }
 
