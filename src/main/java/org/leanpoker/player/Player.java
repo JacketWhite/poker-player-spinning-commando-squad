51c3e236be7f742c55f83494c717310ab472e2b6
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-01-05 11:07:56 +0100

    data hunting

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,21 +1,21 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement gameState) {
+        JsonObject gameStateObject = gameState.getAsJsonObject();
+        JsonArray players = gameStateObject.getAsJsonArray("players");
+        JsonObject player = players.get(gameStateObject.get("in_action").getAsInt()).getAsJsonObject();
 
-        for(JsonElement player: gameState.getAsJsonObject().getAsJsonArray("players")){
-            if (player.getAsJsonObject().get("id") == gameState.getAsJsonObject().get("in_action")){
-                for (JsonElement card : player.getAsJsonObject().getAsJsonArray("hole_cards")){
+                for (JsonElement card : player.getAsJsonArray("hole_cards")){
                     card.getAsJsonObject().get("rank");
                 }
-            }
-
-        }
 
         return 1;
     }
